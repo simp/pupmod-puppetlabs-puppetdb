98d136d2c7af52ef93f1db2687bbba7afb4ed7b8
Trevor Vaughan <tvaughan@onyxpoint.com>
2015-10-22 13:26:29 -0400

    (SIMP-536) Update to upstream 5.0.0
    Updated to the upstream 5.0.0 release of the module
    
    Modified $postgres_version to 'undef' in params.pp so that the defaults
    can take care of things properly.
    
    SIMP-536 #close #comment Update to upstream 5.0.0

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,7 +16,7 @@ class puppetdb::params inherits puppetdb::globals {
   $database                  = $puppetdb::globals::database
   $manage_dbserver           = true
   $manage_pg_repo            = true
-  $postgres_version          = '9.4'
+  $postgres_version          = undef
 
   # The remaining database settings are not used for an embedded database
   $database_host      = 'localhost'
