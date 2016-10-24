6fe4334d6cef4725d1d0d93b0b2cca599feeb0bf
nmarkowski <nmarkowski@keywcorp.com>
2016-10-24 14:50:08 -0400

    (SIMP-1726) Puppetdb updates
    Got spec working with SIMP helpers.
    
    SIMP-1726 #comment done in pdb
    SIMP-1732 #close

--- spec/unit/classes/server/global_ini_spec.rb
+++ spec/unit/classes/server/global_ini_spec.rb
@@ -2,39 +2,44 @@ require 'spec_helper'
 
 describe 'puppetdb::server::global', :type => :class do
   context 'on a supported platform' do
-    let (:facts) do
-      {
-        :osfamily => 'RedHat',
-        :fqdn     => 'foo.com',
-      }
-    end
+    on_supported_os.each do |os, os_facts|
+      let(:facts) do
+        os_facts
+      end
+      context "on #{os}" do
 
-    it { should contain_class('puppetdb::server::global') }
+        describe 'it should compile with all deps, but upstream makes a poor use of require => File["${confdir}/config.ini"]' do
+          pending("is_expected.to compile.with_all_deps")
+        end
 
-    describe 'when using default values' do
-      it { should contain_ini_setting('puppetdb_global_vardir').
-        with(
-          'ensure' => 'present',
-          'path' => 'opt/puppetlabs/server/data/puppetdb',
-          'path' => '/etc/puppetlabs/puppetdb/conf.d/config.ini',
-          'section' => 'global',
-          'setting' => 'vardir',
-          'value' => '/opt/puppetlabs/server/data/puppetdb'
-      )}
+        it { is_expected.to contain_class('puppetdb::server::global') }
 
-    end
+        describe 'when using default values' do
+          it { is_expected.to contain_ini_setting('puppetdb_global_vardir').
+            with(
+              'ensure' => 'present',
+              'path' => 'opt/puppetlabs/server/data/puppetdb',
+              'path' => '/etc/puppetlabs/puppetdb/conf.d/config.ini',
+              'section' => 'global',
+              'setting' => 'vardir',
+              'value' => '/opt/puppetlabs/server/data/puppetdb'
+          )}
 
-    describe 'when using a legacy puppetdb version' do
-      let (:pre_condition) { 'class { "puppetdb::globals": version => "2.2.0", }' }
-      it {should contain_ini_setting('puppetdb_global_vardir').
-        with(
-          'ensure' => 'present',
-          'path' => 'opt/puppetlabs/server/data/puppetdb',
-          'path' => '/etc/puppetdb/conf.d/config.ini',
-          'section' => 'global',
-          'setting' => 'vardir',
-          'value' => '/var/lib/puppetdb'
-      )}
+        end
+
+        describe 'when using a legacy puppetdb version' do
+          let (:pre_condition) { 'class { "puppetdb::globals": version => "2.2.0", }' }
+          it {is_expected.to contain_ini_setting('puppetdb_global_vardir').
+            with(
+              'ensure' => 'present',
+              'path' => 'opt/puppetlabs/server/data/puppetdb',
+              'path' => '/etc/puppetdb/conf.d/config.ini',
+              'section' => 'global',
+              'setting' => 'vardir',
+              'value' => '/var/lib/puppetdb'
+          )}
+        end
+      end
     end
   end
 end
--- spec/unit/classes/server_spec.rb
+++ spec/unit/classes/server_spec.rb
@@ -1,67 +1,103 @@
 require 'spec_helper'
 
 describe 'puppetdb::server', :type => :class do
-  basefacts =
-      {
-        :osfamily                 => 'RedHat',
-        :operatingsystem          => 'RedHat',
-        :operatingsystemrelease   => '6.5',
-        :fqdn                     => 'test.domain.local',
-        :kernel                   => 'Linux',
-      }
-
   context 'on a supported platform' do
-    let(:facts) do
-      basefacts
-    end
+    on_supported_os.each do |os, os_facts|
+      let(:facts) do
+        os_facts
+      end
+      context "on #{os}" do
 
-    it { should contain_class('puppetdb::server') }
+        describe 'with default args' do
+          it { is_expected.to contain_class('puppetdb::server') }
+          it { is_expected.to compile.with_all_deps }
+        end
 
-    describe 'when not specifying JAVA_ARGS' do
-      it { should_not contain_ini_subsetting('Xms') }
-    end
+        describe 'when not specifying JAVA_ARGS' do
+          it { is_expected.to_not contain_ini_subsetting('Xms') }
+        end
 
-    describe 'when specifying JAVA_ARGS' do
-      let(:params) do
-        {
-          'java_args' => {
-            '-Xms' => '2g',
+
+          describe 'when specifying JAVA_ARGS' do
+            let(:params) do
+            {
+              'java_args' => {'-Xms' => '2g'},
+            }
+            end
+            context 'on standard PuppetDB' do 
+              it do
+                if ['RedHat', 'Suse', 'Archlinux'].include?(facts[:osfamily])
+                  is_expected.to contain_ini_subsetting("'-Xms'").with({
+                    'ensure'            => 'present',
+                    'path'              => '/etc/sysconfig/puppetdb',
+                    'section'           => '',
+                    'key_val_separator' => '=',
+                    'setting'           => 'JAVA_ARGS',
+                    'subsetting'        => '-Xms',
+                    'value'             => '2g'
+                  })
+                elsif ['Debian'].include?(facts[:osfamily])
+                  is_expected.to contain_ini_subsetting("'-Xms'").with({
+                    'ensure'            => 'present',
+                    'path'              => '/etc/default/puppetdb',
+                    'section'           => '',
+                    'key_val_separator' => '=',
+                    'setting'           => 'JAVA_ARGS',
+                    'subsetting'        => '-Xms',
+                    'value'             => '2g'
+                  })
+                elsif ['OpenBSD', 'FreeBSD'].include?(facts[:osfamily])
+                  is_expected.to contain_ini_subsetting("'-Xms'").with({
+                    'ensure'            => 'present',
+                    'path'              => 'undef',
+                    'section'           => '',
+                    'key_val_separator' => '=',
+                    'setting'           => 'JAVA_ARGS',
+                    'subsetting'        => '-Xms',
+                    'value'             => '2g'
+                  })
+                end
+              end
+            end
+          end
+
+        describe 'when specifying JAVA_ARGS with merge_default_java_args false' do
+          let (:params) do
+          {
+            'java_args' => {'-Xms' => '2g'},
+            'merge_default_java_args' => false,
           }
-        }
-      end
-
-      context 'on standard PuppetDB' do
-        it { should contain_ini_subsetting("'-Xms'").
-        with(
-          'ensure'            => 'present',
-          'path'              => '/etc/sysconfig/puppetdb',
-          'section'           => '',
-          'key_val_separator' => '=',
-          'setting'           => 'JAVA_ARGS',
-          'subsetting'        => '-Xms',
-          'value'             => '2g'
-        )}
-      end
-
-    end
-
-    describe 'when specifying JAVA_ARGS with merge_default_java_args false' do
-      let (:params) do
-        {
-          'java_args' => {'-Xms' => '2g'},
-          'merge_default_java_args' => false,
-        }
-      end
-
-      context 'on standard PuppetDB' do
-        it { should contain_ini_setting('java_args').
-          with(
-            'ensure' => 'present',
-            'path' => '/etc/sysconfig/puppetdb',
-            'section' => '',
-            'setting' => 'JAVA_ARGS',
-            'value' => '"-Xms2g"'
-        )}
+          end
+          context 'on standard PuppetDB' do
+            it do
+              if ['RedHat', 'Suse', 'Archlinux'].include?(facts[:osfamily])
+                is_expected.to contain_ini_setting('java_args').with({
+                  'ensure'  => 'present',
+                  'path'    => '/etc/sysconfig/puppetdb',
+                  'section' => '',
+                  'setting' => 'JAVA_ARGS',
+                  'value'   => '"-Xms2g"'
+                })
+              elsif ['Debian'].include?(facts[:osfamily])
+                is_expected.to contain_ini_setting('java_args').with({
+                  'ensure'  => 'present',
+                  'path'    => '/etc/default/puppetdb',
+                  'section' => '',
+                  'setting' => 'JAVA_ARGS',
+                  'value'   => '"-Xms2g"'
+                })
+              elsif ['OpenBSD','FreeBSD'].include?(facts[:osfamily])
+                is_expected.to contain_ini_setting('java_args').with({
+                  'ensure'  => 'present',
+                  'path'    => 'undef',
+                  'section' => '',
+                  'setting' => 'JAVA_ARGS',
+                  'value'   => '"-Xms2g"'
+                })
+              end
+            end
+          end
+        end
       end
     end
   end
